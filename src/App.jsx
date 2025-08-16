676cb9a36e300774a448d10885518165012f084d
Subhas2003 <subhasmunain@gmail.com>
2025-08-16 20:40:36 +0530

    Add the date

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,18 @@ function App() {
     settodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
   }
 
+   
+      const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+      useEffect(() => {
+        const intervalId = setInterval(() => {
+          setCurrentDateTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+      }, []);
+
+
   const updateTodo = (id, todo) => {
     settodos((prev) => prev.map((prevtodo) => (prevtodo.id === id ? todo : prevtodo)))
   }
@@ -44,7 +56,13 @@ function App() {
     <Todoprovider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className='flex justify-around '> 
+            <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+
+          <h1 
+          // onChange={time}
+          className='text-2xl rounded-2xl font-bold text-center mb-8 mt-2'>{currentDateTime.toLocaleTimeString()}</h1></div>
+         
           <div className="mb-4">
             <TodoForm />
           </div>
